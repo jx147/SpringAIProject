08d25c4e6bd283032f0f5b7100a4b462582767f2
jx147 <845477256@qq.com>
2026-06-02 15:43:51 +0800

    推送远程

---
+++ src/test/java/org/example/springaiproject/SpringAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springaiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
