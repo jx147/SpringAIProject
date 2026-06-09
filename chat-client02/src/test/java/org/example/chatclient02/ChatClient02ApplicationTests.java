dfc2023245884ce162aaebe1307750383d5ab0a8
jx147 <845477256@qq.com>
2026-06-09 18:20:55 +0800

    chat-client02 test

--- chat-client02/src/test/java/org/example/chatclient02/ChatClient02ApplicationTests.java
+++ chat-client02/src/test/java/org/example/chatclient02/ChatClient02ApplicationTests.java
@@ -1,13 +1,33 @@
 package org.example.chatclient02;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 @SpringBootTest
 class ChatClient02ApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testChatClient(@Autowired ChatClient.Builder chatClientBuilder) {
+        String content = chatClientBuilder.build()
+                .prompt()
+                .user("你好")
+                .call()
+                .content();
+
+        System.out.println(content);
     }
 
+    @Test
+    void testStreamChatClient(@Autowired ChatClient.Builder chatClientBuilder) {
+        Flux<String> content = chatClientBuilder.build()
+                .prompt()
+                .user("你好")
+                .stream()
+                .content();
+
+        content.toIterable().forEach(System.out::println);
+    }
 }
